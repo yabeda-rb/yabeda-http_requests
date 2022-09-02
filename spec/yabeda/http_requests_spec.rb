95032d4c9454904d2c4d3d8f583d26e921e024a9
Mathieu Martin <mat.martin@iheartjane.com>
2022-09-02 09:06:42 -0400

    Ensure http method is always downcased

--- spec/yabeda/http_requests_spec.rb
+++ spec/yabeda/http_requests_spec.rb
@@ -13,16 +13,16 @@ RSpec.describe Yabeda::HttpRequests do
 
     it 'holds the proper data' do
       expect(Yabeda.http_request_total.values).to(
-        eq({ host: 'sushi.com', method: 'GET', port: 80 } => 1)
+        eq({ host: 'sushi.com', method: 'get', port: 80 } => 1)
       )
       expect(Yabeda.http_response_total.values.keys.first).to(
         include({
-                  host: 'sushi.com', method: 'GET', port: 80,
+                  host: 'sushi.com', method: 'get', port: 80,
                   status: 301
                 })
       )
       expect(Yabeda.http_response_duration.values.keys.first).to(
-        eq(host: 'sushi.com', port: 80, method: 'GET', status: 301)
+        eq(host: 'sushi.com', port: 80, method: 'get', status: 301)
       )
     end
   end
